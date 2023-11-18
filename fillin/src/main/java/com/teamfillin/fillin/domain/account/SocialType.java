29e82d33561fccdbdb123cb771e3d2185f22e0d6
chanwoo <chocw0402@gmail.com>
2023-11-19 00:12:22 +0900

    Social Type enum

--- fillin/src/main/java/com/teamfillin/fillin/domain/account/SocialType.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/account/SocialType.java
@@ -1,18 +1,5 @@
-package com.teamfillin.fillin.domain.user;
+package com.teamfillin.fillin.domain.account;
 
 public enum SocialType {
-
-	APPLE("애플"),
-	KAKAO("카카오"),
-	;
-
-	private final String value;
-
-	SocialType(String value) {
-		this.value = value;
-	}
-
-	public String getValue() {
-		return value;
-	}
+	APPLE, KAKAO;
 }
