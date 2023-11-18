9817aa5ccd1abca8c95b78c5200b8365e415f99f
chanwoo <chocw0402@gmail.com>
2023-11-19 00:18:00 +0900

    Studio 엔티티 구성

--- fillin/src/main/java/com/teamfillin/fillin/domain/studio/Studio.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/studio/Studio.java
@@ -1,2 +1,47 @@
-package com.teamfillin.fillin.domain.studio;public class Studio {
+package com.teamfillin.fillin.domain.studio;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Studio {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	@Column(length = 500)
+	private String address;
+
+	@Column(length = 50)
+	private String tel;
+
+	private Double latitude;
+
+	private Double longitude;
+
+	@Column(columnDefinition = "TEXT")
+	private String etc;
+
+	@Column(length = 500)
+	private String site;
+
+	protected Studio() {
+	}
+
+	public Studio(String name, String address, String tel, Double latitude, Double longitude, String etc, String site) {
+		this.name = name;
+		this.address = address;
+		this.tel = tel;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.etc = etc;
+		this.site = site;
+	}
 }
