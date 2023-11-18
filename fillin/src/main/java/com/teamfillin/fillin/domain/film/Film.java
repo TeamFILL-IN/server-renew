e458c3460465506b64df5a820226178e131fe4df
chanwoo <chocw0402@gmail.com>
2023-11-19 00:13:26 +0900

    Film 엔티티 구성

--- fillin/src/main/java/com/teamfillin/fillin/domain/film/Film.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/film/Film.java
@@ -1,2 +1,33 @@
-package com.teamfillin.fillin.domain.film;public class Film {
+package com.teamfillin.fillin.domain.film;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Film {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@Column(nullable = false, length = 100)
+	private String name;
+
+	@Column(nullable = false)
+	private Long typeNo;
+
+	@Column(nullable = false)
+	private Long companyNo;
+
+	protected Film() {
+	}
+
+	public Film(String name, Long typeNo, Long companyNo) {
+		this.name = name;
+		this.typeNo = typeNo;
+		this.companyNo = companyNo;
+	}
 }
