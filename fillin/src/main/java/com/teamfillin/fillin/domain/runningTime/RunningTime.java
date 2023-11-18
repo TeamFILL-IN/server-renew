c39c53c27d6ccb232a11cfe3ba7729440c20a52a
chanwoo <chocw0402@gmail.com>
2023-11-19 00:17:09 +0900

    RunningTime 엔티티 구성

--- fillin/src/main/java/com/teamfillin/fillin/domain/runningTime/RunningTime.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/runningTime/RunningTime.java
@@ -1,2 +1,33 @@
-package com.teamfillin.fillin.domain.runningTime;public class RunningTime {
+package com.teamfillin.fillin.domain.runningTime;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RunningTime {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@Column(nullable = false)
+	private Long studioNo;
+
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Day dayOfWeek;
+
+	private LocalDateTime startAt;
+
+	private LocalDateTime endAt;
+
+	protected RunningTime() {
+	}
 }
