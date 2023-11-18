5f6c92956f84e1a2cff781eb5229093ca7bef028
chanwoo <chocw0402@gmail.com>
2023-11-19 00:13:58 +0900

    Company 엔티티 구성

--- fillin/src/main/java/com/teamfillin/fillin/domain/film/Company.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/film/Company.java
@@ -1,2 +1,21 @@
-package com.teamfillin.fillin.domain.film;public class Company {
+package com.teamfillin.fillin.domain.film;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	protected Company() {
+	}
 }
