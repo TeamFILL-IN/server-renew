e63fb76d8827c61097dcc9a492c5e9e5f595862f
머환임 <udsward@gmail.com>
2023-11-19 15:27:29 +0900

    OAuth2 dependency 제거

--- fillin/build.gradle.kts
+++ fillin/build.gradle.kts
@@ -31,7 +31,6 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
