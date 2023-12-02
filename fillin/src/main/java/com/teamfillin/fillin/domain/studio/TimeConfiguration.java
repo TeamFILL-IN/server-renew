0b7c47796062f042d90ed31ca52f95ddd4c2f6ca
chanwoo <chocw0402@gmail.com>
2023-12-03 02:50:00 +0900

    korea time zone 설정

--- fillin/src/main/java/com/teamfillin/fillin/domain/studio/TimeConfiguration.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/studio/TimeConfiguration.java
@@ -1,2 +1,18 @@
-package com.teamfillin.fillin.domain.studio;public class TimeConfiguration {
+package com.teamfillin.fillin.domain.studio;
+
+import java.time.Clock;
+import java.time.ZoneId;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TimeConfiguration {
+
+	private static final ZoneId KOREA_TIME_ZONE = ZoneId.of("Asia/Seoul");
+
+	@Bean
+	public Clock koreaTimeClock() {
+		return Clock.system(KOREA_TIME_ZONE);
+	}
 }
