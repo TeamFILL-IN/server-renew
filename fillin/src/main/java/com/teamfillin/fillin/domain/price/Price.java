b105942e362c379443b71686db621b6c592f3fe7
chanwoo <chocw0402@gmail.com>
2023-12-03 02:37:19 +0900

    studio 상세 조회를 위한 price 로직

--- fillin/src/main/java/com/teamfillin/fillin/domain/price/Price.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/price/Price.java
@@ -1,2 +1,43 @@
-package com.teamfillin.fillin.domain.price;public class Price {
+package com.teamfillin.fillin.domain.price;
+
+import java.util.Objects;
+
+public class Price {
+
+	private final long studioNo;
+	private final String name;
+	private final int amount;
+
+	private Price(long studioNo, String name, int amount) {
+		this.studioNo = studioNo;
+		this.name = name;
+		this.amount = amount;
+	}
+
+	public static Price from(PriceEntity priceEntity) {
+		return new Price(priceEntity.getStudioNo(), priceEntity.getName(), priceEntity.getAmount());
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Price price = (Price)o;
+		return studioNo == price.studioNo && amount == price.amount && Objects.equals(name, price.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(studioNo, name, amount);
+	}
 }
