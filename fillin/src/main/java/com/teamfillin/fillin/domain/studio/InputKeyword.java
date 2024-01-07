837edd0d51c827e9cfb75af985cd3c9809b9a160
chanwoo <chocw0402@gmail.com>
2024-01-07 14:40:00 +0900

    사용자 입력 검색어 관리 객체

--- fillin/src/main/java/com/teamfillin/fillin/domain/studio/InputKeyword.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/studio/InputKeyword.java
@@ -1,21 +1,39 @@
-package com.teamfillin.fillin.api.studio;
+package com.teamfillin.fillin.domain.studio;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Objects;
 
 public class InputKeyword {
 
 	private static final String SPLITTER = " ";
 
-	private final String value;
+	private final List<String> values;
 
-	public InputKeyword(String value) {
-		this.value = value;
+	private InputKeyword(List<String> values) {
+		this.values = values;
 	}
 
-	public List<String> getInputKeywordTokens() {
-		return Arrays.stream(value.split(SPLITTER))
-			.collect(Collectors.toList());
+	public static InputKeyword from(String keyword) {
+		return new InputKeyword(Arrays.asList((keyword.split(SPLITTER))));
+	}
+
+	public List<String> getValues() {
+		return values;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		InputKeyword that = (InputKeyword)o;
+		return Objects.equals(values, that.values);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(values);
 	}
 }
