694bd3381877ea617e260ccfd3495f6113395092
chanwoo <chocw0402@gmail.com>
2024-01-07 14:44:26 +0900

    studio 검색 로직

--- fillin/src/main/java/com/teamfillin/fillin/domain/studio/StudioRepository.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/studio/StudioRepository.java
@@ -1,8 +1,17 @@
 package com.teamfillin.fillin.domain.studio;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudioRepository extends JpaRepository<StudioEntity, Long> {
+
+	@Query(value =
+		"SELECT s.* FROM studio s "
+			+ "WHERE MATCH(s.name, s.address) "
+			+ "AGAINST(:searchKeyword IN BOOLEAN MODE)", nativeQuery = true)
+	List<StudioEntity> searchByKeywordInBooleanMode(String searchKeyword);
 }
--- fillin/src/main/java/com/teamfillin/fillin/domain/studio/StudioSearchResult.java
+++ fillin/src/main/java/com/teamfillin/fillin/domain/studio/StudioSearchResult.java
@@ -1,2 +1,30 @@
-package com.teamfillin.fillin.domain.studio;public class StudioSearchResult {
+package com.teamfillin.fillin.domain.studio;
+
+public class StudioSearchResult {
+
+	private final long id;
+	private final String name;
+	private final String address;
+
+	private StudioSearchResult(long id, String name, String address) {
+		this.id = id;
+		this.name = name;
+		this.address = address;
+	}
+
+	public static StudioSearchResult from(Studio studio) {
+		return new StudioSearchResult(studio.getNo(), studio.getName(), studio.getAddress());
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
 }
